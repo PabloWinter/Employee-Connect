8700287d8eb5e77d26c09ab543ceeceffb6bef03
Brijesh Patel <b.patel405@mybvc.ca>
2019-03-10 17:02:46 -0600

    sign up form created

--- EmployeeConnect/app/src/main/java/ca/bvc/employeeconnect/MainActivity.java
+++ EmployeeConnect/app/src/main/java/ca/bvc/employeeconnect/MainActivity.java
@@ -15,15 +15,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        loginImage = (ImageView)findViewById(R.id.imageView_id);
-        //loginImage.setImageResource(R.drawable.login_image);
-        logInButton = (Button)findViewById(R.id.login_button_id);
-        logInButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, Navigation.class);
-                startActivity(intent);
-            }
-        });
+//        loginImage = (ImageView)findViewById(R.id.imageView_id);
+//        //loginImage.setImageResource(R.drawable.login_image);
+//        logInButton = (Button)findViewById(R.id.login_button_id);
+//        logInButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(MainActivity.this, Navigation.class);
+//                startActivity(intent);
+//            }
+//        });
+    }
+
+    public void openSignUp(View view) {
+        Intent signUpIntent = new Intent(this, SignUp.class);
+        startActivity(signUpIntent);
+    }
+
+    public void authenticateUser(View view) {
+        Intent logInIntent = new Intent(this, Navigation.class);
+        startActivity(logInIntent);
     }
 }
