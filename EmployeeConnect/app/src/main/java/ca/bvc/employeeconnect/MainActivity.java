a130a2b3baab10e0acf88795d80536bcd6b1695c
sandeep saini <s.saini516@mybvc.ca>
2019-03-09 20:59:59 -0700

    UI phase for schedule

--- EmployeeConnect/app/src/main/java/ca/bvc/employeeconnect/MainActivity.java
+++ EmployeeConnect/app/src/main/java/ca/bvc/employeeconnect/MainActivity.java
@@ -1,13 +1,29 @@
 package ca.bvc.employeeconnect;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageView loginImage;
+    Button logInButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        loginImage = (ImageView)findViewById(R.id.imageView_id);
+        loginImage.setImageResource(R.drawable.login_image);
+        logInButton = (Button)findViewById(R.id.login_button_id);
+        logInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(".ScheduleActivity");
+                startActivity(intent);
+            }
+        });
     }
 }
