34f4a18d340b0d9fae069444cfdfc4d7293c38d2
p.rodriguez673 <p.rodriguez673@mybvc.ca>
2019-03-29 13:38:24 -0600

    Added FireStore database connection, and modified sign up form

--- EmployeeConnect/app/src/main/java/ca/bvc/employeeconnect/SignUp.java
+++ EmployeeConnect/app/src/main/java/ca/bvc/employeeconnect/SignUp.java
@@ -1,9 +1,19 @@
 package ca.bvc.employeeconnect;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class SignUp extends AppCompatActivity {
 
@@ -11,6 +21,10 @@ public class SignUp extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        // FireStore instance database
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
     }
 
     public void openLogin(View view) {
